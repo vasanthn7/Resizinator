af721286cffbfab1ef36bd4645e5c5cec5007fad
Vasanth <vasanthn7@gmail.com>
2024-05-18 22:38:56 +0530

    Return signed URLs and added API Docs

--- resizinator/image/models.py
+++ resizinator/image/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from resizinator.storage_backends import PrivateMediaStorage
 
 # Create your models here.
 class Image(models.Model):
-    original = models.ImageField(upload_to='')
-    small = models.ImageField(upload_to='', blank=True, null=True)
-    medium = models.ImageField(upload_to='', blank=True, null=True)
+    original = models.ImageField(upload_to='', storage=PrivateMediaStorage)
+    small = models.ImageField(upload_to='', storage=PrivateMediaStorage, blank=True, null=True)
+    medium = models.ImageField(upload_to='', storage=PrivateMediaStorage, blank=True, null=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
--- resizinator/resizinator/settings.py
+++ resizinator/resizinator/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'storages',
+    'drf_yasg',
     'image',
     'user'
 ]
