c657b81ae639f95bf7b3052de9965b8d3c738468
Vasanth <vasanthn7@gmail.com>
2024-05-19 07:32:19 +0530

    Minor changes

--- resizinator/resizinator/settings.py
+++ resizinator/resizinator/settings.py
@@ -132,9 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+STATIC_ROOT = '/var/www/static'
 
 MEDIA_URL = "media/"
+MEDIA_ROOT = "/var/www/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
