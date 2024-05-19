09ee10af334dc0815f36d697f07f66098eeeb1e1
Vasanth <vasanthn7@gmail.com>
2024-05-19 12:57:07 +0530

    Fixed image propotion

--- resizinator/image/tasks.py
+++ resizinator/image/tasks.py
@@ -6,6 +6,9 @@ from django.core.files.storage import default_storage
 
 from image.models import Image
 
+SMALL_HEIGHT = 100
+MEDIUM_HEIGHT = 300
+
 @shared_task
 def resize_image(image_id):
     image = Image.objects.get(id=image_id)
@@ -13,14 +16,14 @@ def resize_image(image_id):
         image_data = BytesIO(f.read())
     original = Img.open(image_data)
 
-    small = original.resize((100, 100))
+    small = original.resize((round(original.width/(original.height/SMALL_HEIGHT)), SMALL_HEIGHT))
     small_io = BytesIO()
     small.save(small_io, format=original.format)
     with default_storage.open(f'small_{image.original.name}', 'wb') as f:
         f.write(small_io.getvalue())
     image.small.save(f'small_{image.original.name}', small_io)
 
-    medium = original.resize((300, 300))
+    medium = original.resize((round(original.width/(original.height/MEDIUM_HEIGHT)), MEDIUM_HEIGHT))
     medium_io = BytesIO()
     medium.save(medium_io, format=original.format)
     with default_storage.open(f'medium_{image.original.name}', 'wb') as f:
